b8105ec59e01589df42387da189e6e43c9b3ef8e
mirnavazquez <mirnavrl@mail.com>
2022-01-25 17:26:05 -0600

    My CV

---
+++ R/2021_08_17-CV.R
@@ -0,0 +1,15 @@
+#remotes::install_github('mitchelloharawild/icons@v0.1.0')
+#devtools::install_github("nstrayer/datadrivencv")
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Mirna Vazquez Rosas Landa",
+  data_location = "CV/data/",
+  pdf_location = "https://github.com/mirnavazquez/MVRL_cv/CV/MVRL_cv.pdf",
+  html_location = "https://github.com/mirnavazquez/MVRL_cv/CV/",
+  source_location = "https://github.com/mirnavazquez/MVRL_cv/R/"
+)
+
+# install.packages("devtools")
+devtools::install_github("tidyverse/googlesheets4", force = T)
+# install.packages("devtools")
+devtools::install_github("r-lib/gargle")
